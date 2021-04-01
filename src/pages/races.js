fc08e119785a1366bda0a83ce0f5d598c0d3ec60
Kelson Adams <kadams@governmentcio.com>
2021-04-01 11:09:12 -0600

    Add perks and start subraces

--- src/pages/races.js
+++ src/pages/races.js
@@ -1,8 +1,9 @@
 // Node modules.
-import * as React from "react"
+import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import map from "lodash/map"
+import uniq from "lodash/uniq"
 // Relative imports.
 import defaultRaceImage from "../images/defaultRace.png"
 import Layout from "../components/layout"
@@ -12,7 +13,7 @@ const Wrapper = styled.section`
   align-items: center;
   display: flex;
   flex-direction: column;
-  margin: 20px 0 0;
+  margin: 90px 0 0;
   text-align: center;
 
   h2 {
@@ -60,6 +61,10 @@ const Wrapper = styled.section`
       }
     }
 
+    h4 {
+      margin: 20px 0 0;
+    }
+
     .fields {
       display: flex;
       flex-flow: row wrap;
@@ -98,20 +103,40 @@ const RacesPage = () => {
             climbSpeed
             flySpeed
             hitPoints
+            id
             imageURL
             landSpeed
             languages
             name
+            perkIDs
             size
             swimSpeed
+          }
+          SUBRACES {
+            description
+            id
+            name
+            penalty
+            raceID
+            requirements
+          }
+          PERKS {
+            description
+            id
+            name
+            requirements
           }
         }
       }
     }
   `)
 
-  // Derive Races data from the graphql query above.
-  const RACES = queryResult?.site.siteMetadata.RACES
+  // Derive Races and Perks data from the graphql query above.
+  const RACES = queryResult?.site?.siteMetadata?.RACES
+  const SUBRACES = queryResult?.site?.siteMetadata?.SUBRACES
+  const PERKS = queryResult?.site?.siteMetadata?.PERKS
+
+  const [expandedPerkIDs, setExpandedPerkIDs] = useState([])
 
   return (
     <Layout>
@@ -121,17 +146,21 @@ const RacesPage = () => {
         <ul>
           {map(RACES, race => {
             // Derive race properties.
-            const name = race?.name
-            const imageURL = race?.imageURL || defaultRaceImage
-            const size = race?.size
-            const landSpeed = race?.landSpeed
-            const swimSpeed = race?.swimSpeed
+            const ability = race?.ability
+            const burrowSpeed = race?.burrowSpeed
             const climbSpeed = race?.climbSpeed
             const flySpeed = race?.flySpeed
-            const burrowSpeed = race?.burrowSpeed
             const hitPoints = race?.hitPoints
+            const id = race?.id
+            const imageURL = race?.imageURL || defaultRaceImage
+            const landSpeed = race?.landSpeed
             const languages = race?.languages?.join(", ")
-            const ability = race?.ability
+            const name = race?.name
+            const perkIDs = race?.perkIDs
+            const size = race?.size
+            const swimSpeed = race?.swimSpeed
+
+            // Derive the subraces.
 
             return (
               <li key={name}>
@@ -139,6 +168,8 @@ const RacesPage = () => {
                   <img alt={`${name} race`} src={imageURL} />
                   <h3>{name}</h3>
                 </header>
+
+                <h4>Core Stats</h4>
 
                 <section className="fields">
                   <section className="field-group">
@@ -152,29 +183,37 @@ const RacesPage = () => {
                   </section>
 
                   <section className="field-group">
-                    <p className="label">Swim Speed</p>
-                    <p className="value">{swimSpeed}</p>
-                  </section>
-
-                  <section className="field-group">
-                    <p className="label">Climb Speed</p>
-                    <p className="value">{climbSpeed}</p>
-                  </section>
-
-                  <section className="field-group">
-                    <p className="label">Fly Speed</p>
-                    <p className="value">{flySpeed}</p>
-                  </section>
-
-                  <section className="field-group">
-                    <p className="label">Burrow Speed</p>
-                    <p className="value">{burrowSpeed}</p>
-                  </section>
-
-                  <section className="field-group">
                     <p className="label">Hit Points</p>
                     <p className="value">{hitPoints}</p>
                   </section>
+
+                  {swimSpeed > 0 && (
+                    <section className="field-group">
+                      <p className="label">Swim Speed</p>
+                      <p className="value">{swimSpeed}</p>
+                    </section>
+                  )}
+
+                  {climbSpeed > 0 && (
+                    <section className="field-group">
+                      <p className="label">Climb Speed</p>
+                      <p className="value">{climbSpeed}</p>
+                    </section>
+                  )}
+
+                  {flySpeed > 0 && (
+                    <section className="field-group">
+                      <p className="label">Fly Speed</p>
+                      <p className="value">{flySpeed}</p>
+                    </section>
+                  )}
+
+                  {burrowSpeed > 0 && (
+                    <section className="field-group">
+                      <p className="label">Burrow Speed</p>
+                      <p className="value">{burrowSpeed}</p>
+                    </section>
+                  )}
                 </section>
 
                 {ability && (
@@ -187,6 +226,46 @@ const RacesPage = () => {
                 <section className="field-group">
                   <p className="label">Languages</p>
                   <p className="value">{languages}</p>
+                </section>
+
+                <section className="subraces"></section>
+
+                <section className="perks">
+                  <h4>Perks</h4>
+                  {map(perkIDs, perkID => {
+                    // Derive the perk.
+                    const perk = PERKS?.[perkID]
+                    const description = perk?.description
+                    const name = perk?.name
+                    const requirements = perk?.requirements
+
+                    return (
+                      <section className="perk" key={`${id}-${perkID}`}>
+                        {/* Perk Name */}
+                        <h5
+                          onClick={() =>
+                            setExpandedPerkIDs(
+                              uniq([...expandedPerkIDs, perkID])
+                            )
+                          }
+                        >
+                          {name}
+                        </h5>
+
+                        {/* Expanded Perk Info */}
+                        {expandedPerkIDs?.includes(perkID) && (
+                          <>
+                            {requirements && (
+                              <p>
+                                <strong>Requirements:</strong> {requirements}
+                              </p>
+                            )}
+                            <p>{description}</p>
+                          </>
+                        )}
+                      </section>
+                    )
+                  })}
                 </section>
               </li>
             )
