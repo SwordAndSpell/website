29f2b167b4efc358b6c02413aade32402b7bc466
Kelson Adams <kadams@governmentcio.com>
2021-04-02 21:42:03 -0600

    Add cardsPage reusable Wrapper component for all pages

--- src/pages/core-identities.js
+++ src/pages/core-identities.js
@@ -1,7 +1,6 @@
 // Node modules.
 import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import styled, { keyframes } from "styled-components"
 import find from "lodash/find"
 import filter from "lodash/filter"
 import map from "lodash/map"
@@ -11,161 +10,7 @@ import Chevron from "../components/icons/Chevron"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import defaultCoreIdentityImage from "../images/defaultCoreIdentity.jpg"
-
-const fadeIn = keyframes`
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-`
-
-const Wrapper = styled.section`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  margin: 90px 0 0;
-  text-align: center;
-
-  h2 {
-    text-align: center;
-    width: 100%;
-  }
-
-  ul {
-    align-items: center;
-    display: flex;
-    flex-flow: row wrap;
-  }
-
-  li {
-    align-items: center;
-    background: #ffffff;
-    box-shadow: 0 4px 8px 0 rgb(0, 0, 0, 0.4);
-    display: flex;
-    flex-direction: column;
-    margin: 0 20px 50px;
-    width: 100%;
-
-    * {
-      animation: ${fadeIn} 0.5s;
-    }
-
-    &:nth-of-type(even) {
-      h3 {
-        background: rgba(164, 0, 255, 0.8);
-      }
-    }
-
-    header {
-      align-items: center;
-      display: flex;
-      flex-direction: column;
-      position: relative;
-      outline: none;
-      width: 100%;
-
-      img {
-        width: 100%;
-      }
-
-      h3 {
-        align-items: center;
-        background: rgba(81, 43, 252, 0.8);
-        bottom: 0;
-        color: #ffffff;
-        display: flex;
-        font-size: 2.5rem;
-        justify-content: space-between;
-        line-height: 40px;
-        padding: 10px 20px;
-        position: absolute;
-        text-align: left;
-        width: 100%;
-      }
-
-      .chevron {
-        height: 35px;
-        margin-left: 20px;
-        transition: transform 0.5s ease;
-
-        path {
-          fill: #ffffff;
-        }
-
-        &.expanded {
-          transform: rotate(180deg);
-        }
-      }
-    }
-
-    h4 {
-      margin: 20px 0 0;
-    }
-
-    .fields {
-      display: flex;
-      flex-flow: row wrap;
-      justify-content: center;
-      padding: 10px 10px 0;
-    }
-
-    .field-group {
-      display: flex;
-      flex-direction: column;
-      margin: 10px;
-
-      .label {
-        font-size: 1rem;
-        font-weight: bold;
-        margin: 0;
-
-        &:first-of-type {
-          margin: 20px 0;
-        }
-      }
-
-      .value {
-        color: #444444;
-        font-size: 1.2rem;
-        margin: 0;
-        white-space: pre-line;
-        text-align: left;
-      }
-    }
-
-    .core-abilities {
-      align-items: center;
-      display: flex;
-      flex-direction: column;
-      margin-bottom: 20px;
-      padding: 0 20px;
-      width: 100%;
-
-      header {
-        margin: 0 0 20px;
-      }
-
-      .disclaimer {
-        color: #999999;
-        font-size: 0.7rem;
-        margin: 0;
-      }
-
-      .core-ability {
-        white-space: pre-line;
-        text-align: left;
-
-        h5 {
-          color: #512bfccc;
-          font-size: 1.5rem;
-          text-align: center;
-        }
-      }
-    }
-  }
-`
+import { Wrapper } from "../components/cardsPage"
 
 const onCollapseToggle = (id, expandedIDs, setExpandedIDs) => {
   // Expand the ID.
@@ -277,22 +122,22 @@ const CoreIdentitiesPage = () => {
                     {/* ========== */}
                     <section className="fields">
                       <section className="field-group">
-                        <p className="label">Health at Level 1</p>
+                        <h4>Health at Level 1</h4>
                         <p className="value">{healthAtFirstLevel}</p>
                       </section>
 
                       <section className="field-group">
-                        <p className="label">Health at Level 2+</p>
+                        <h4>Health at Level 2+</h4>
                         <p className="value">{healthBeyondFirstLevel}</p>
                       </section>
 
                       <section className="field-group">
-                        <p className="label">Starting Equipment</p>
+                        <h4>Starting Equipment</h4>
                         <p className="value">{startingEquipment}</p>
                       </section>
 
                       <section className="field-group">
-                        <p className="label">Initial Proficiencies</p>
+                        <h4>Initial Proficiencies</h4>
                         <p className="value">{initialProficiencies}</p>
                       </section>
                     </section>
@@ -302,7 +147,7 @@ const CoreIdentitiesPage = () => {
                     {/* CORE_IDENTITIES */}
                     {/* ========== */}
                     {coreAbilities?.length > 0 && (
-                      <section className="core-abilities">
+                      <section className="collapsibles">
                         <header>
                           <h4>Core Abilities</h4>
                         </header>
@@ -318,7 +163,7 @@ const CoreIdentitiesPage = () => {
 
                           return (
                             <button
-                              className="core-ability"
+                              className="collapsible"
                               key={coreIdentityAbilityID}
                               onClick={() =>
                                 onCollapseToggle(
