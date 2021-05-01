8be58d5986b0e8ad7a7815f7c4172a19dbb4d1c5
Kelson Adams <kadams@governmentcio.com>
2021-04-30 18:45:07 -0600

    Remove gatsby-plugin-offline for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -174,12 +174,12 @@ module.exports = {
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/`, `/pages/*`],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    //   options: {
+    //     precachePages: [`/`, `/pages/*`],
+    //   },
+    // },
     // Full Story integration
     {
       resolve: `gatsby-plugin-fullstory`,
