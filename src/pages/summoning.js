29c2fefe09026556086a82916062ff04fc62de9e
Sam Suddath <srsuddath@gmail.com>
2021-05-11 23:47:50 -0400

    add summons

---
+++ src/pages/summoning.js
@@ -0,0 +1,87 @@
+// Node modules.
+import React, { useState } from "react"
+import { useStaticQuery, graphql } from "gatsby"
+// Relative imports.
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import { Wrapper } from "../components/cardsPage"
+import { onCollapseToggle } from "../utils"
+import StatBlock from "../components/StatBlock"
+
+const SummoningPage = () => {
+  const queryResult = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          SUMMONABLES {
+            abilities {
+              description
+              name
+            }
+            actions {
+              description
+              name
+            }
+            armor
+            charisma
+            constitution
+            defense
+            dexterity
+            fortitude
+            hitPoints
+            id
+            intelligence
+            immunities
+            name
+            senses
+            reflex
+            resistances
+            speed
+            strength
+            type
+            will
+            wisdom
+          }
+        }
+      }
+    }
+  `)
+
+  // Derive Races and Perks data from the graphql query above.
+  const SUMMONABLES = queryResult?.site?.siteMetadata?.SUMMONABLES
+  const [expandedBlocksIDs, setExpandedBlocksIDs] = useState([])
+  return (
+    <Layout>
+      <Seo title="Summons" />
+      <Wrapper>
+        <h2>Summonable Creatures</h2>
+
+        {/* begin headings for abilities */}
+        <ul>
+          {SUMMONABLES?.map(summon => {
+            // Derive if the details are expanded.
+            const id = summon?.id
+            const isExpanded = expandedBlocksIDs?.includes(id)
+            return (
+              <li key={id}>
+                <StatBlock
+                  data={summon}
+                  isOpen={isExpanded}
+                  clickHandler={() =>
+                    onCollapseToggle(
+                      id,
+                      expandedBlocksIDs,
+                      setExpandedBlocksIDs
+                    )
+                  }
+                />
+              </li>
+            )
+          })}
+        </ul>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export default SummoningPage
