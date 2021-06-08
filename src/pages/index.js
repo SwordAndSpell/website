0bb31136cef061e0d553e3d451b0a0a52a12f7b3
Sam Suddath <srsuddath@gmail.com>
2021-06-08 13:44:43 -0400

    Major release

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 // Relative imports.
-import flameVideo from "../../static/videos/flame.mp4"
+// import flameVideo from "../../static/videos/flame.mp4"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -10,10 +10,10 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <section className="homepage">
-      <video autoPlay loop muted playsInline type="video/mp4">
+      {/* <video autoPlay loop muted playsInline type="video/mp4">
         <source src={flameVideo} type="video/mp4" />
         Your browser does not support the video tag.
-      </video>
+      </video> */}
       <h1>Embark on your journey</h1>
       <Link to="/races">Choose your race</Link>
     </section>
